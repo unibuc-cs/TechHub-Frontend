90561e03c3d638e82a540f13d7007c2f7f80c85a
codru <eusebiu.timofte@my.fmi.unibuc.ro>
2020-12-10 17:03:46 +0200

    finished register ui, logic with dummy saga and added validation to register inputs

--- src/pages/Authenication/SignUp.tsx
+++ src/pages/Authenication/SignUp.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+/* eslint-disable no-control-regex */
+import React, { useEffect, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -10,7 +11,7 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 
 import { useDispatch } from "react-redux";
-import { getUserInformation } from "../../store/user/user.actions";
+import { sendRegisterInformation } from "../../store/user/user.actions";
 
 import { Link } from "react-router-dom";
 
@@ -47,6 +48,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/;
+
 const SignUp = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
@@ -56,24 +60,105 @@ const SignUp = () => {
   const [password, setPassword] = useState<string>("");
   const [confirmPassword, setConfirmPassword] = useState<string>("");
 
+  const [emailError, setEmailError] = useState<string>("");
+  const [usernameError, setUsernameError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>("");
+
+  const [emailTouched, setEmailTouched] = useState<boolean>(false);
+  const [usernameTouched, setUsernameTouched] = useState<boolean>(false);
+  const [passwordTouched, setPasswordTouched] = useState<boolean>(false);
+  const [confirmPasswordTouched, setConfirmPasswordTouched] = useState<boolean>(
+    false
+  );
+  const [signUpIsDisabled, setSignUpIsDisabled] = useState<boolean>(true);
+
   const onEmailChangedHandler = (e: any) => {
     setEmail(e.target.value);
+    setEmailTouched(true);
   };
 
   const onUsernameChangedHandler = (e: any) => {
     setUsername(e.target.value);
+    setUsernameTouched(true);
   };
 
   const onPasswordChangedHandler = (e: any) => {
     setPassword(e.target.value);
+    setPasswordTouched(true);
   };
 
   const onConfirmPasswordChangedHandler = (e: any) => {
     setConfirmPassword(e.target.value);
+    setConfirmPasswordTouched(true);
   };
 
+  useEffect(() => {
+    if (emailTouched) {
+      if (emailRegex.test(email)) {
+        setEmailError("");
+      } else {
+        setEmailError("Email is not valid");
+      }
+    }
+
+    if (usernameTouched) {
+      if (username.trim().length > 0) {
+        setUsernameError("");
+      } else {
+        setUsernameError("Username must not be empty");
+      }
+    }
+
+    if (passwordTouched) {
+      if (passwordRegex.test(password)) {
+        setPasswordError("");
+      } else {
+        setPasswordError(
+          "Password must be at least 6 characters long and contain lowercase, uppercase and number"
+        );
+      }
+    }
+
+    if (confirmPasswordTouched) {
+      if (confirmPassword === password) {
+        setConfirmPasswordError("");
+      } else {
+        setConfirmPasswordError("Must match password");
+      }
+    }
+
+    if (
+      emailTouched &&
+      usernameTouched &&
+      passwordTouched &&
+      confirmPasswordTouched &&
+      emailError === "" &&
+      usernameError === "" &&
+      passwordError === "" &&
+      confirmPasswordError === ""
+    ) {
+      setSignUpIsDisabled(false);
+    } else {
+      setSignUpIsDisabled(true);
+    }
+  }, [
+    email,
+    password,
+    username,
+    confirmPassword,
+    emailTouched,
+    usernameTouched,
+    passwordTouched,
+    confirmPasswordTouched,
+    emailError,
+    usernameError,
+    passwordError,
+    confirmPasswordError,
+  ]);
+
   const onSignUpPressedHandler = () => {
-    dispatch(getUserInformation(email, password));
+    dispatch(sendRegisterInformation(email, username, password));
   };
 
   return (
@@ -100,6 +185,8 @@ const SignUp = () => {
               autoComplete="email"
               autoFocus
               onChange={(e) => onEmailChangedHandler(e)}
+              error={emailError !== ""}
+              helperText={emailError}
             />
             <TextField
               variant="outlined"
@@ -110,8 +197,9 @@ const SignUp = () => {
               label="Username"
               name="username"
               autoComplete="username"
-              autoFocus
               onChange={(e) => onUsernameChangedHandler(e)}
+              error={usernameError !== ""}
+              helperText={usernameError}
             />
             <TextField
               variant="outlined"
@@ -124,6 +212,8 @@ const SignUp = () => {
               id="password"
               autoComplete="current-password"
               onChange={(e) => onPasswordChangedHandler(e)}
+              error={passwordError !== ""}
+              helperText={passwordError}
             />
             <TextField
               variant="outlined"
@@ -132,10 +222,12 @@ const SignUp = () => {
               fullWidth
               name="confirm password"
               label="Confirm Password"
-              type="confirm password"
+              type="password"
               id="confirm password"
               autoComplete="current-password"
-              onChange={(e) => onPasswordChangedHandler(e)}
+              onChange={(e) => onConfirmPasswordChangedHandler(e)}
+              error={confirmPasswordError !== ""}
+              helperText={confirmPasswordError}
             />
             <Button
               fullWidth
@@ -143,6 +235,7 @@ const SignUp = () => {
               color="primary"
               className={classes.submit}
               onClick={onSignUpPressedHandler}
+              disabled={signUpIsDisabled}
             >
               Sign Up
             </Button>
